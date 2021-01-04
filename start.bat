9e81bee3bf0176110136fc1c059b1df6eb0e9387
Sheng Feng <sfeng77@gmail.com>
2021-01-03 17:05:15 -0800

    Update bat

--- start.bat
+++ start.bat
@@ -1,3 +1,7 @@
 @echo off
-python "downloadandsetwallpaper.py"
-pause
+cd  /d  C:\Users\sfeng\Projects\RefreshWallpaper
+set Log=C:\Users\sfeng\Projects\RefreshWallpaper\Log.txt
+echo Script launched on %date% at %time% under %UserName% > %Log%
+c:\python\python39\python.exe  C:\Users\sfeng\Projects\RefreshWallpaper\downloadandsetwallpaper.py  1>>%Log%  2>>&1
+echo Script ended on %date% at %time% >> %Log%
+
